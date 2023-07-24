dbca7360c17edb6a5e94c9b90baa24be48dc3626
Tanagends <tanagends@gmail.com>
2023-07-24 12:05:40 -0700

    printf

---
+++ README.md
@@ -0,0 +1,2 @@
+Welcome to my printf project.
+Look around.
---
+++ main.h
@@ -0,0 +1,7 @@
+#ifndef MACRO_H
+#define MACRO_H
+#include <stdarg.h>
+#include <stdlib.h>
+#define BUFF-SIZE 1024
+int _printf(const char *format, ...);
+#endif
